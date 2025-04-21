f9942199231d7f174defc4bfd6504394bce955a1
AaronTan11 <tanweihup@gmail.com>
2025-04-21 19:04:29 +0800

    chore: Update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false }
     return config
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   }
 };
 
